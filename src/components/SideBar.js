c076705f51b881ebc44fd014e1a2e7c0ebbea32b
Stenger2 <91592999+Stenger2@users.noreply.github.com>
2024-05-18 12:26:57 +0300

    first commit

---
+++ src/components/SideBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {Box, Drawer, Divider, IconButton, List, ListItem, ListItemButton , ListItemIcon, ListItemText} from  "@mui/material"
+import Logo from "../img/logo.png"
+
+export default function SideBar() {
+  // const location = useLocation();
+  // const navigate = useNavigate()
+  return (
+    <div>
+      <span className="logo">ArrowFlicks</span>
+    </div>
+  );
+}
